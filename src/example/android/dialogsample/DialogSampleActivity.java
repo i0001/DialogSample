ca02d69db8baa7e4a4c8efcb7f1e84b92bf8154f
i0001 <il0venature1008@gmail.com>
2014-05-22 14:51:30 +0900

    修正１

--- src/example/android/dialogsample/DialogSampleActivity.java
+++ src/example/android/dialogsample/DialogSampleActivity.java
@@ -140,17 +140,17 @@ public class DialogSampleActivity extends Activity {
 		public void showDatePickerDialog() {
 			Calendar cal = Calendar.getInstance();
 
-			DatePickerDialog dialog
-				= new DatePickerDialog(DialogSampleActivity.this,
-						new DatePickerDialog.OnDateSetListener() {
-							@Override
-							public void onDateSet(DatePicker picker, int year, int month,int day) {
-								label.setText("日付選択ダイアログ："+ year +"年"+(month+1)+"月"+ day +"日");
-							}
+			DatePickerDialog dialog = new DatePickerDialog(DialogSampleActivity.this
+					,new DatePickerDialog.OnDateSetListener() {
+						public void onDateSet(DatePicker picker,
+											int year, int month, int day) {
+						label.setText("日付選択ダイアログ：" +
+										year + "年" + (month+1) + "月" + day + "日");
 						}
-						,cal.get(Calendar.YEAR)
-						,cal.get(Calendar.MONTH)
-						,cal.get(Calendar.DAY_OF_YEAR)
+					}
+					,cal.get(Calendar.YEAR)
+					,cal.get(Calendar.MONTH)
+					,cal.get(Calendar.DAY_OF_MONTH)
 			);
 			dialog.show();
 		}
